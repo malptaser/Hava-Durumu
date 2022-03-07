df9300b57474fb59346f9222a83c9bc699e650a9
M.Alp <96910379+malptaser@users.noreply.github.com>
2022-03-07 20:09:38 +0300

    WeatherPageViewController ayarlamaları yapıldı.

--- Hava Durumu/Controlers/WeatherPageViewController.swift
+++ Hava Durumu/Controlers/WeatherPageViewController.swift
@@ -7,25 +7,64 @@
 import UIKit
 import CoreLocation
 
-class WeatherPageViewController: UIPageViewController {
+class WeatherPageViewController: UIPageViewController, CLLocationManagerDelegate {
+    
+    enum Appearance: Int {
+        case daily
+        case weekly
+    }
+    
+    
+    
     
     // MARK: - UI Elements
     
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
+    lazy var dailyViewController: DailyViewController = {
+        return storyboard!.instantiateViewController(identifier: "DailyViewController")
+    }()
+    lazy var weeklyViewController: WeeklyViewController = {
+        return storyboard!.instantiateViewController(identifier: "WeeklyViewController")
+    }()
+    var managedControllers: [UIViewController] {
+        return [dailyViewController, weeklyViewController]
+    }
     
     
     // MARK: - Properties
     
+    let locationManager = CLLocationManager()
+    var currentAppearance: Appearance = .daily
+    
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        locationManager.delegate = self
+        locationManager.requestLocation()
+    
+        updateCurrentController(animated: false)
     }
     
     // MARK: - Functions
     
-    // MARK: Actions
-    @IBAction func segmentedControl(_ sender: Any) {
+    func updateCurrentController(animated: Bool) {
+        switch currentAppearance {
+        case .daily:
+            setViewControllers([dailyViewController], direction: .reverse, animated: animated, completion: nil)
+        case .weekly:
+            setViewControllers([weeklyViewController], direction: .forward, animated: animated, completion: nil)
+        }
     }
+    
+    // MARK: Actions
+    @IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        if let newAppearance = Appearance(rawValue: segmentedControl.selectedSegmentIndex) {
+            currentAppearance = newAppearance
+            updateCurrentController(animated: true)
+        }
+    }
+    
 }
 
