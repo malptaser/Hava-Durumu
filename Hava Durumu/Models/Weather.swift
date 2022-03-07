849761ad1fc312354f79170708d751934c8f27a6
M.Alp <96910379+malptaser@users.noreply.github.com>
2022-03-07 19:58:30 +0300

    API kodlamaları tamamlandı.

--- Hava Durumu/Models/Weather.swift
+++ Hava Durumu/Models/Weather.swift
@@ -10,5 +10,12 @@ import Foundation
 struct Weather: Codable {
     
     // MARK: - Properties
+    var cityName: String
+        var data: [DailyWeather]
+        
+        enum CodingKeys: String, CodingKey {
+            case cityName = "city_name"
+            case data = "data"
+        }
 }
 
